2f733ee3c07318ae86c5dad7d27fbe4f195b5869
Pedro Trovo <98984115+TrovoPedro@users.noreply.github.com>
2023-11-27 14:58:39 -0300

    Add files via upload

---
+++ UserPost.js
@@ -0,0 +1,78 @@
+import React from 'react';
+import {View, Text, TextInput, TouchableOpacity} from 'react-native';
+
+const MultilineTextInputExample = () => {
+  const [value, onChangeText] = React.useState('');
+  const [username, setUsername] = React.useState('');
+  const [email, setEmail] = React.useState('');
+  const [password, setPassword] = React.useState('');
+  const [count, setCount] = React.useState('');
+  const onPress = () => setCount(prevCount => prevCount);
+
+  function handleSubmit(event){
+    event.preventDefault(); 
+
+    console.log({
+      username,
+      email,
+      password
+    });
+    
+    fetch('https://dogsapi.origamid.dev/json/api/user', {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+  },
+  body: JSON.stringify({
+    username,
+    email,
+    password
+  }),
+  }).then(response => {
+    console.log(response);
+    return response.json();
+  }).then(json => {
+    console.log(json);
+    return json;
+  })
+}
+  return (
+    <View>
+
+      <TextInput
+      id='usuario'
+        placeholder="Usuario"
+        onChange={({target}) => setUsername(target.value)}
+        value={username}
+        style={{padding: 10}}
+      />
+      <Text>{username}</Text>
+     
+      <TextInput
+      id='email'
+        placeholder="email"
+        onChange={({target}) => setEmail(target.value)}
+        value={email}
+        style={{padding: 10}}
+      />
+      <Text>{email}</Text>
+     
+      <TextInput
+      id='password'
+        placeholder="senha"
+        secureTextEntry={true}
+        onChange={({target}) => setPassword(target.value)}
+        value={password}
+        style={{padding: 10}}
+      />
+      <Text>{password}</Text>
+      <TouchableOpacity  onPress={handleSubmit}>
+        <Text>Press Here</Text>
+      </TouchableOpacity>
+     
+    </View>
+  );
+};
+
+export default MultilineTextInputExample;
